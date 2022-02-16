2e316306300449f8f097c8761d8f2592f580c7a3
yamzil <yousef.amzil@digimind.com>
2022-02-16 12:37:00 +0100

    initial commit

---
+++ src/hashmap/CustomHashMap.java
@@ -0,0 +1,56 @@
+package hashmap;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * @author yamzil
+ * @created 2022-02-15
+ * @project map
+ */
+public class CustomHashMap<K, V> {
+
+    private Entry<K, V>[] bucket;
+    private static final int BUCKET_CAPACITY = 17;
+
+    public CustomHashMap() {
+        this.bucket = new Entry[BUCKET_CAPACITY];
+    }
+
+    public void put(K key, V value) {
+        if(key == null)
+            return;
+        int keyHashCode = this.getHashCode(key);
+        int index = keyHashCode % BUCKET_CAPACITY;
+        Entry<K, V> element = new Entry<>(key, value, null);
+        if(bucket[index] == null){
+            bucket[index] = element;
+        } else{
+            Entry<K, V> previous = bucket[index];
+            while(previous.next != null){
+                previous = previous.next;
+            }
+            previous.next = element;
+        }
+    }
+
+    public void displayMap(){
+        System.out.println(Arrays.toString(this.bucket));
+    }
+
+    private int getHashCode(K key) {
+        return 17 * 37 + Objects.hashCode(key);
+    }
+    class Entry<K, V>{
+        protected K key;
+        protected V value;
+        protected Entry<K, V> next;
+
+        public Entry(K key, V value, Entry<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
+
